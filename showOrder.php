f37062207c2a7a36e4a3156df25cf01f03bc6e62
Mohaiminul Islam <76485709+MuhitiNix@users.noreply.github.com>
2023-12-02 23:49:04 +0600

    Add files via upload
    Upload

---
+++ showOrder.php
@@ -0,0 +1,50 @@
+<?php
+include 'config.php';
+session_start();
+$statement="select * from orders";
+$result=mysqli_query($conn, $statement);
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Orders</title>
+    <link rel="stylesheet" href="css/showorderstyle.css">
+    <link rel="stylesheet" href="css/headerstyle.css">
+</head>
+<body>
+
+<?php
+echo "<h2>Recent Orders</h2>";
+
+echo "<table>";
+echo "<tr>
+    <th>Name</th>
+    <th>Number</th>
+    <th>Email</th>
+    <th>package</th>
+    <th>Address</th>
+    <th>Total Price</th>
+</tr>";
+while ($row = mysqli_fetch_assoc($result)) {
+    echo "<tr>";
+    echo "<td>" . $row['name'] . "</td>";
+    echo "<td>" . $row['number'] . "</td>";
+    echo "<td>" . $row['email'] . "</td>";
+    echo "<td>" . $row['method'] . "</td>";
+    echo "<td>" . $row['address'] . "</td>";
+    echo "<td>" . $row['total_price'] . "</td>";
+    echo "</tr>";
+}
+echo "</table>";
+
+
+mysqli_close($conn);
+  
+ ?>
+
+
+</body>
+</html>
